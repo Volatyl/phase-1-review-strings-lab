f2b058ff8cf0b4ac336c180086296782a0c514f2
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-13 12:58:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !`;
+
+let shortGreeting = "Welcome, ";
+
+const firstInitial = currentUser[0];
+
+shortGreeting = `Welcome, ${firstInitial} !`;
+
+console.log(shortGreeting);
